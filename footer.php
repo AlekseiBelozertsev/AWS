fe30a479e2c6383c1570475c959b053e6862cb6d
Aleksei Belozertsev <aleksei.belozertsev@conquest.cz>
2022-05-02 12:45:15 +0200

    first commit here

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 		<footer class="navbar navbar-default">
 			<div class="container">
-				<?php echo '<p class="navbar-text text-center">' . __( '<a target="_blank" href="http://www.hardeepasrani.com/portfolio/photos/">Photos</a> ~ Proudly powered by WordPress', 'photos' ) . '</p>'; ?>
+				
 			</div>
 		</footer>
 	</div>
