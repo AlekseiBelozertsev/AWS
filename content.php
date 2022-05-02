077a910621275ff725beaeabb7b546e17bd973a5
Aleksei Belozertsev <aleksei.belozertsev@conquest.cz>
2022-05-02 15:08:02 +0200

    keep deleting

--- content.php
+++ content.php
@@ -1,6 +1,6 @@
 
 		<div id="post-<?php the_ID(); ?>" <?php post_class('photogrid-item'); ?>>
-			<a class="photogrid-link" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+			
 			<?php if ( has_post_thumbnail() ) : ?>
 				<?php $feat_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
 				<img class="featured-image" src="<?php echo $feat_image; ?>" />
