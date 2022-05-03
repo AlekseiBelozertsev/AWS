4ce827be9c5077bd0e4f9f0d756fceb6f060b079
Aleksei Belozertsev <aleksei.belozertsev@conquest.cz>
2022-05-03 16:32:17 +0200

    trying to display images

--- content.php
+++ content.php
@@ -1,7 +1,7 @@
 
 		<div id="post-<?php the_ID(); ?>" <?php post_class('photogrid-item'); ?>>
 			<?php if ( has_post_thumbnail()) : ?>
-				<?php $feat_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
+				<?php $feat_image = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID) ); ?>
 				<img class="featured-image" src="<?php echo $feat_image; ?>" />
 			<?php else: ?>
 				<?php $feat_image = get_stylesheet_directory_uri() . '/assets/images/default.jpg'; ?>
