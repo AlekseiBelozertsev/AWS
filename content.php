6114769c7af1bbe59f3b7d853d3deb64420e70bc
Aleksei Belozertsev <aleksei.belozertsev@conquest.cz>
2022-05-03 16:24:02 +0200

    fffffff

--- content.php
+++ content.php
@@ -4,7 +4,7 @@
 				<?php $feat_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
 				<img class="featured-image" src="<?php echo $feat_image; ?>" />
 			<?php else: ?>
-				<?php $feat_image =  wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
+				<?php $feat_image = get_stylesheet_directory_uri() . '/assets/images/default.jpg'; ?>
 				<img class="featured-image" src="<?php echo $feat_image; ?>" />
 			<?php endif; ?>
 			</a>
